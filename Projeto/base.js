d54fc132253e7a0a5204fc87387066e2f14e7aa8
Otto Schmidt <ottoschmidt23@gmail.com>
2023-08-11 19:11:18 -0300

    paginas principais finalizadas

--- Projeto/base.js
+++ Projeto/base.js
@@ -6,6 +6,8 @@ function alterarTab(indice) {
         ordem = ["certificados", "diploma", "biblioteca", "historico", "horario", "webestagio", "eventos"];
     } else if (dir == "financeiro/") {
         ordem = ["taxas", "parcelas", "oferta", "divida"];
+    } else if (dir == "documentos/") {
+        ordem = ["academicos", "financeiros", "ensino"];
     }
 
     if (!tabs[indice].classList.contains("selecionado")) {
@@ -18,4 +20,16 @@ function alterarTab(indice) {
 
         document.getElementById("mostrar").setAttribute("src", dir + ordem[indice] + ".html");
     }
-};
+}
+
+function atualizarBarras () {
+    barras = document.getElementsByClassName("progresso");
+
+    if (barras.length != 0) {
+        for (i = 0; i < barras.length; i++) {
+            valor = barras[i].getAttribute("data-progresso");
+
+            barras[i].style.width = valor + "%";
+        }
+    }
+}
