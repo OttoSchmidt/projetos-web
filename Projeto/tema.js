f99af6a0a376fef48d9490a05c18a8e0e10bf8b1
Otto Schmidt <ottoschmidt23@gmail.com>
2023-08-17 06:55:53 -0300

    abc

--- Projeto/tema.js
+++ Projeto/tema.js
@@ -1,16 +1,21 @@
 //https://web.dev/building-a-theme-switch-component/ ---> MUDAR TEMA
 
 const nomeChave = "tema";
+const nomeCor = "cor";
 
-const recuperarPreferencia = () => {
-    valor = localStorage.getItem(nomeChave);
+function recuperarPreferencia () {
+    valorCor = localStorage.getItem(nomeCor);
 
+    //recupera o tema do sistema
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) 
         localStorage.setItem(nomeChave, "dark");
     else if (window.matchMedia("(prefers-color-scheme: light)").matches) 
         localStorage.setItem(nomeChave, "light");
 
+    if (valorCor == null) localStorage.setItem(nomeCor, "0");
+
     document.firstElementChild.setAttribute("data-tema", localStorage.getItem(nomeChave));
+    document.firstElementChild.setAttribute("data-cor", localStorage.getItem(nomeCor));
 }
 
 if (localStorage.getItem(nomeChave) == null) recuperarPreferencia();
