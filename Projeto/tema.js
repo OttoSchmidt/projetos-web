e09732768a17bbfa9f378b0581324c4f17d16953
Otto Schmidt <ottoschmidt23@gmail.com>
2023-08-21 14:40:40 -0300

    adicionado outras cores

--- Projeto/tema.js
+++ Projeto/tema.js
@@ -4,32 +4,35 @@ const nomeChave = "tema";
 const nomeCor = "cor";
 
 function recuperarPreferencia () {
-    valorCor = localStorage.getItem(nomeCor);
-
     //recupera o tema do sistema
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) 
         localStorage.setItem(nomeChave, "dark");
     else if (window.matchMedia("(prefers-color-scheme: light)").matches) 
         localStorage.setItem(nomeChave, "light");
 
-    if (valorCor == null) localStorage.setItem(nomeCor, "0");
-
     document.firstElementChild.setAttribute("data-tema", localStorage.getItem(nomeChave));
     document.firstElementChild.setAttribute("data-cor", localStorage.getItem(nomeCor));
 }
 
 if (localStorage.getItem(nomeChave) == null) recuperarPreferencia();
+if (localStorage.getItem(nomeCor) == null) localStorage.setItem(nomeCor, "0");
 
 document.firstElementChild.setAttribute("data-tema", localStorage.getItem(nomeChave));
+document.firstElementChild.setAttribute("data-cor", localStorage.getItem(nomeCor));
 
 function alterarTema(indice) {
-    opcoesDisponiveis = ["sistema", "dark", "light"];
+    const opcoesDisponiveis = ["sistema", "dark", "light"];
     valor = opcoesDisponiveis[indice];
 
     if (indice == 0) {
         recuperarPreferencia();
     } else {
         document.firstElementChild.setAttribute("data-tema", valor);
-        localStorage.setItem("tema", valor);
+        localStorage.setItem(nomeChave, valor);
     }
+}
+
+function mudarCores (indice) {
+    document.firstElementChild.setAttribute("data-cor", indice);
+    localStorage.setItem(nomeCor, indice);
 }
