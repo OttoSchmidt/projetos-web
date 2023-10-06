2626840087c60c740ace4e96b7599623b0213881
Otto <otto.schmidt@cs.up.edu.br>
2023-10-06 19:30:04 -0300

    modificacoes basico

--- bibliotecas-c/js/indice.js
+++ bibliotecas-c/js/indice.js
@@ -1,12 +1,14 @@
+//carrega todos os elementos relevantes em variaveis
 const h2 = document.querySelectorAll("h2");
 const indiceLista = document.getElementById("indice").querySelectorAll("li");
 
+//funcao para atualizar o destaque do indice
 window.addEventListener("scroll", ()=> {
-    let atual = 0;
-
-    document.getElementById("selecionado").id = "";
+    let atual = 0; //inicia a variavel
+    document.getElementById("selecionado").id = ""; //remove o destaque atual
 
     for (let i = 0; i < h2.length; i++) {
+        //verifica se o scrollY (o scroll atual da pagina) é maior que a posicao Y do h2[i] em relacao ao topo da pagina menos 1/3 da altura da janela
         if (scrollY > h2[i].offsetTop - window.innerHeight * 0.3) {
             atual = i;
             document.getElementById("indice").scrollTo(0, i * indiceLista[0].offsetHeight);
@@ -15,17 +17,20 @@ window.addEventListener("scroll", ()=> {
         }
     }
 
+    //atualiza o destaque do indice
     indiceLista[atual].id = "selecionado";
 });
 
+//funcao para pesquisa
 function procurar() {
-    texto = document.getElementById("barraPesquisa").value;
+    texto = document.getElementById("barraPesquisa").value.toLowerCase(); //valor do input em letras minusculas (compatibilidade)
     navLista = document.getElementById("indice").getElementsByTagName("li");
 
+    //loop para cada elemento na lista de indices
     for (let item of navLista) {
-        conteudo = item.getElementsByTagName("a")[0].textContent;
-        texto = texto.toLowerCase();
+        conteudo = item.getElementsByTagName("a")[0].textContent; //recebe o conteudo do elemento atual do loop
 
+        //verificar se o elemento atual do loop contem o valor do input
         if (conteudo.includes(texto)) {
             item.classList.remove("esconder");
         } else {
--- bibliotecas-c/js/tema.js
+++ bibliotecas-c/js/tema.js
@@ -1,11 +1,29 @@
-//alterar o tema da pagina
+//pegar o valor do cookie
+temaPreferencia = document.cookie.split('=')[1];
 
+//funcao do botao para alterar o tema
 function alterarTema () {
-    if (document.body.classList.contains("modoEscuro")) {
-        document.body.classList.remove("modoEscuro");
+    if (document.documentElement.getAttribute('data-tema') === 'dark') {
+        document.documentElement.setAttribute('data-tema', 'light');
         document.getElementById("btnTema").innerText = "dark_mode";
+        document.cookie = "tema=";
     } else {
-        document.body.classList.add("modoEscuro");
+        document.documentElement.setAttribute('data-tema', 'dark');
         document.getElementById("btnTema").innerText = "light_mode";
+        document.cookie = "tema=dark";
     }
 }
+
+/*quando a pagina é aberta (sem o html carregar), é atribuido um atributo ao elemento HTML de acordo com o cookie
+(assim, a pagina não pisca quando carrega) */
+if (temaPreferencia === 'dark') {
+    document.documentElement.setAttribute('data-tema', 'dark');
+} else {
+    document.documentElement.setAttribute('data-tema', 'light');
+}
+
+//quando o html carregar, caso a pagina esteja em modo noturno, altera o icone do botao
+document.addEventListener('DOMContentLoaded',function() {
+    if (document.documentElement.getAttribute('data-tema') === 'dark')
+        document.getElementById("btnTema").innerText = "light_mode";
+});
