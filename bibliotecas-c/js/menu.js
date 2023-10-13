70d94945311c34279a0c0c6944ac0cac9053e2f7
Otto <otto.schmidt@cs.up.edu.br>
2023-10-13 12:17:11 -0300

    comentarios

--- bibliotecas-c/js/menu.js
+++ bibliotecas-c/js/menu.js
@@ -1,13 +1,15 @@
 function abrirMenu() {
-    menu = document.getElementById("navBibliotecas");
-    menu.classList.toggle("ativo");
+    const menu = document.getElementById("navBibliotecas");
+    menu.classList.toggle("ativo"); /*se o elemento já tem a classe ativo, ele remove, caso contrario, ele adiciona*/
 
     if (menu.classList.contains("ativo")) {
-        remover = document.createElement("div");
+        //cria o fundo para remover o menu do nav
+        const remover = document.createElement("div");
         remover.id = "removerMenu";
         remover.setAttribute("onclick", "abrirMenu();");
         document.body.appendChild(remover);
     } else {
+        //remove o fundo criado anteriormente
         document.getElementById("removerMenu").remove();
     }
 }
